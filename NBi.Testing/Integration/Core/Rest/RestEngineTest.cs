3f80559594aa6fdb1747e168ca834895ac333243
Cédric L. Charlier <cedric.charlier@yucom.be>
2017-03-30 20:55:03 +0200

    Replace GitHub calls by GoogleBooks calls for integration tests

--- NBi.Testing/Integration/Core/Rest/RestEngineTest.cs
+++ NBi.Testing/Integration/Core/Rest/RestEngineTest.cs
@@ -45,42 +45,63 @@ namespace NBi.Testing.Integration.Core.Rest
         }
 
         [Test]
-        public void Execute_ValidCallForGithub_ExpectedResult()
+        public void Execute_ValidCallForGoogleBooks_ExpectedResult()
         {
             var factory = new RestClientFactory();
-            var client = factory.Instantiate(ContentType.Json, "https://api.github.com/", CredentialsType.Anonymous);
+            var client = factory.Instantiate(ContentType.Json, "https://www.googleapis.com/books/v1/", CredentialsType.Anonymous);
             var engine = new RestEngine(client);
 
             var parameters = new Dictionary<string, string>();
-            parameters.Add("username", "Seddryck");
+            parameters.Add("isbn", "0747532699");
 
-            var dataset = engine.Execute("users/$username$", parameters);
+            var dataset = engine.Execute("volumes?q=isbn:$isbn$", parameters);
 
             Assert.That(dataset, Is.Not.Null);
             Assert.That(dataset.Tables, Has.Count.EqualTo(1));
             Assert.That(dataset.Tables[0].Rows, Has.Count.EqualTo(1));
-            Assert.That(dataset.Tables[0].Columns, Has.Count.EqualTo(30));
-            Assert.That(dataset.Tables[0].Rows[0].ItemArray[28], Is.EqualTo(new DateTime(2013,9,13,21,6,29)));
+            Assert.That(dataset.Tables[0].Columns, Has.Count.EqualTo(3));
+
+            Assert.That(dataset.Tables[0].Rows[0].ItemArray[2], Is.TypeOf<DataTable>());
+            var items = dataset.Tables[0].Rows[0].ItemArray[2] as DataTable;
+
+            Assert.That(items.Rows[0].ItemArray[4], Is.TypeOf<DataTable>());
+            var volumeInfo = items.Rows[0].ItemArray[4] as DataTable;
+
+            Assert.That(volumeInfo.Rows[0].ItemArray[1], Is.InstanceOf<IEnumerable<object>>());
+            var authors = volumeInfo.Rows[0].ItemArray[1] as IEnumerable<object>;
+            Assert.That(authors, Has.Count.EqualTo(1));
+
+            Assert.That(volumeInfo.Rows[0].ItemArray[7], Is.TypeOf<int>());
         }
 
         [Test]
-        public void Execute_ValidCallForGithub2_ExpectedResult()
+        public void Execute_ValidCallForGoogleBooks2_ExpectedResult()
         {
             var factory = new RestClientFactory();
-            var client = factory.Instantiate(ContentType.Json, "https://api.github.com/", CredentialsType.Anonymous);
+            var client = factory.Instantiate(ContentType.Json, "https://www.googleapis.com/books/v1/", CredentialsType.Anonymous);
             var engine = new RestEngine(client);
 
             var parameters = new Dictionary<string, string>();
-            parameters.Add("username", "Seddryck");
-            var dataset = engine.Execute("users/$username$/repos", parameters);
+            parameters.Add("q", "isbn:3905829800");
+
+            var dataset = engine.Execute("volumes", parameters);
 
             Assert.That(dataset, Is.Not.Null);
             Assert.That(dataset.Tables, Has.Count.EqualTo(1));
-            Assert.That(dataset.Tables[0].Rows, Has.Count.GreaterThan(15));
+            Assert.That(dataset.Tables[0].Rows, Has.Count.EqualTo(1));
+            Assert.That(dataset.Tables[0].Columns, Has.Count.EqualTo(3));
 
-            Assert.That(dataset.Tables[0].Rows[0][3], Is.TypeOf<DataTable>());
-            var owner = dataset.Tables[0].Rows[0][3] as DataTable;
-            Assert.That(owner.Columns, Has.Count.EqualTo(17));
+            Assert.That(dataset.Tables[0].Rows[0]["items"], Is.TypeOf<DataTable>());
+            var items = dataset.Tables[0].Rows[0]["items"] as DataTable;
+
+            Assert.That(items.Rows[0]["volumeInfo"], Is.TypeOf<DataTable>());
+            var volumeInfo = items.Rows[0]["volumeInfo"] as DataTable;
+
+            Assert.That(volumeInfo.Rows[0]["authors"], Is.InstanceOf<IEnumerable<object>>());
+            var authors = volumeInfo.Rows[0]["authors"] as IEnumerable<object>;
+            Assert.That(authors, Has.Count.EqualTo(2));
+
+            Assert.That(volumeInfo.Rows[0]["pageCount"], Is.TypeOf<int>());
         }
     }
 }
