8808002dd537c0ceedca977fa4f5ae9cc922db1b
Cédric L. Charlier <cedric.charlier@yucom.be>
2017-03-31 00:32:10 +0200

    Basic implementation of the array comparer (taking into consideration the ordering of members)

---
+++ NBi.Core/ResultSet/Comparer/ArrayComparer.cs
@@ -0,0 +1,136 @@
+﻿using NBi.Core.ResultSet.Converter;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NBi.Core.ResultSet.Comparer
+{
+    public class ArrayComparer
+    {
+        private readonly NumericComparer numericComparer = new NumericComparer();
+        private readonly TextComparer textComparer = new TextComparer();
+        private readonly DateTimeComparer dateTimeComparer = new DateTimeComparer();
+        private readonly BooleanComparer booleanComparer = new BooleanComparer();
+
+        public ComparerResult Compare(object x, object y, ColumnType columnType)
+        {
+            //Any management
+            if (x.ToString() != "(any)" && y.ToString() != "(any)")
+            {
+                //Null management
+                if (DBNull.Value.Equals(x))
+                {
+                    if (y is IEnumerable<object>)
+                    {
+                        if (((IEnumerable<object>)y).Count() != 0)
+                            return new ComparerResult("(null)");
+                    }
+                    else if (!DBNull.Value.Equals(y) && y.ToString() != "(null)")
+                        return new ComparerResult("(null)");
+                }
+                else if (DBNull.Value.Equals(y))
+                {
+                    if (x is IEnumerable<object>)
+                    {
+                        if (((IEnumerable<object>)x).Count() != 0)
+                            return new ComparerResult("(array)");
+                    }
+                    if (!DBNull.Value.Equals(x) && x.ToString() != "(null)" && x.ToString() != "(blank)")
+                        return new ComparerResult(x.ToString());
+                }
+                //(value) management
+                else if (x.ToString() == "(value)" || y.ToString() == "(value)")
+                {
+                    if (DBNull.Value.Equals(x) || DBNull.Value.Equals(y))
+                        return new ComparerResult(DBNull.Value.Equals(y) ? "(null)" : x.ToString());
+                    if ((x is IEnumerable<object>) && ((IEnumerable<object>)x).Count() == 0)
+                        return new ComparerResult("(null)");
+                    if ((y is IEnumerable<object>) && ((IEnumerable<object>)y).Count() == 0)
+                        return new ComparerResult("(array)");
+                }
+                //Not Null management
+                else
+                {
+                    //Check the array
+                    if (!(x is IEnumerable<object>))
+                        return new ComparerResult("(cell)");
+                    if (!(y is IEnumerable<object>))
+                        return new ComparerResult("(array)");
+
+                    var xArray = x as IEnumerable<object>;
+                    var yArray = y as IEnumerable<object>;
+
+                    //Check the length of the arrays
+                    if (xArray.Count() != yArray.Count())
+                        return new ComparerResult("(array-size)");
+
+                    Func<object, bool> filter = (o => o.ToString() != "(any)" && o.ToString() != "(value)");
+
+                    //Numeric
+                    if (columnType == ColumnType.Numeric)
+                    {
+                        var converter = new NumericConverter();
+                        foreach (var item in xArray.Union(yArray))
+                            if (!converter.IsValid(item))
+                                return new ComparerResult("(array-type-mismatch)");
+
+                        for (int i = 0; i < xArray.Count(); i++)
+                        {
+                            var res = numericComparer.Compare(xArray.ElementAt(i), yArray.ElementAt(i));
+                            if (!res.AreEqual)
+                                return res;
+                        }
+                    }
+                    //Date and Time
+                    else if (columnType == ColumnType.DateTime)
+                    {
+                        var converter = new DateTimeConverter();
+                        foreach (var item in xArray.Union(yArray))
+                            if (!converter.IsValid(item))
+                                return new ComparerResult("(array-type-mismatch)");
+
+                        for (int i = 0; i < xArray.Count(); i++)
+                        {
+                            var res = dateTimeComparer.Compare(xArray.ElementAt(i), yArray.ElementAt(i));
+                            if (!res.AreEqual)
+                                return res;
+                        }
+                    }
+                    //Boolean
+                    else if (columnType == ColumnType.Boolean)
+                    {
+                        var converter = new BooleanConverter();
+                        foreach (var item in xArray.Union(yArray))
+                            if (!converter.IsValid(item))
+                                return new ComparerResult("(array-type-mismatch)");
+
+                        for (int i = 0; i < xArray.Count(); i++)
+                        {
+                            var res = booleanComparer.Compare(xArray.ElementAt(i), yArray.ElementAt(i));
+                            if (!res.AreEqual)
+                                return res;
+                        }
+                    }
+                    //Text
+                    else
+                    {
+                        var converter = new TextConverter();
+                        foreach (var item in xArray.Union(yArray))
+                            if (!converter.IsValid(item))
+                                return new ComparerResult("(array-type-mismatch)");
+
+                        for (int i = 0; i < xArray.Count(); i++)
+                        {
+                            var res = textComparer.Compare(xArray.ElementAt(i), yArray.ElementAt(i));
+                            if (!res.AreEqual)
+                                return res;
+                        }
+                    }
+                }
+            }
+            return ComparerResult.Equality;
+        }
+    }
+}
---
+++ NBi.Testing/Unit/Core/ResultSet/Comparer/ArrayComparerTest.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Linq;
+using NBi.Core.ResultSet.Comparer;
+using NUnit.Framework;
+
+namespace NBi.Testing.Unit.Core.ResultSet.Comparer
+{
+    [TestFixture]
+    public class ArrayComparerTest
+    {
+        #region SetUp & TearDown
+        //Called only at instance creation
+        [TestFixtureSetUp]
+        public void SetupMethods()
+        {
+
+        }
+
+        //Called only at instance destruction
+        [TestFixtureTearDown]
+        public void TearDownMethods()
+        {
+        }
+
+        //Called before each test
+        [SetUp]
+        public void SetupTest()
+        {
+        }
+
+        //Called after each test
+        [TearDown]
+        public void TearDownTest()
+        {
+        }
+        #endregion
+
+        [Test]
+        public void Compare_SameArray_True()
+        {
+            var comparer = new ArrayComparer();
+            var x = new object[] { "alpha", "beta" };
+            var result = comparer.Compare(x, x, NBi.Core.ResultSet.ColumnType.Text);
+            Assert.That(result.AreEqual, Is.True);
+        }
+
+        [Test]
+        public void Compare_IdenticalArray_True()
+        {
+            var comparer = new ArrayComparer();
+            var x = new object[] { "alpha", "beta" };
+            var y = new object[] { "alpha", "beta" };
+            var result = comparer.Compare(x, y, NBi.Core.ResultSet.ColumnType.Text);
+            Assert.That(result.AreEqual, Is.True);
+        }
+
+        [Test]
+        public void Compare_DifferentArray_False()
+        {
+            var comparer = new ArrayComparer();
+            var x = new object[] { "alpha", "beta" };
+            var y = new object[] { "alpha", "gamma" };
+            var result = comparer.Compare(x, y, NBi.Core.ResultSet.ColumnType.Text);
+            Assert.That(result.AreEqual, Is.False);
+        }
+
+
+        [Test]
+        public void Compare_DifferentLengthArray_False()
+        {
+            var comparer = new ArrayComparer();
+            var x = new object[] { "alpha", "beta" };
+            var y = new object[] { "alpha", "beta", "gamma" };
+            var result = comparer.Compare(x, y, NBi.Core.ResultSet.ColumnType.Text);
+            Assert.That(result.AreEqual, Is.False);
+        }
+
+
+        [Test]
+        public void Compare_IdenticalNumericArray_True()
+        {
+            var comparer = new ArrayComparer();
+            var x = new object[] { "1.120", "2.35" };
+            var y = new object[] { 1.12, 2.35 };
+            var result = comparer.Compare(x, y, NBi.Core.ResultSet.ColumnType.Numeric);
+            Assert.That(result.AreEqual, Is.True);
+        }
+
+        [Test]
+        public void Compare_AnyArray_True()
+        {
+            var comparer = new ArrayComparer();
+            var x = "(any)";
+            var y = new object[] { "alpha", "beta" };
+            var result = comparer.Compare(x, y, NBi.Core.ResultSet.ColumnType.Text);
+            Assert.That(result.AreEqual, Is.True);
+        }
+
+        [Test]
+        public void Compare_ValueArray_True()
+        {
+            var comparer = new ArrayComparer();
+            var x = "(value)";
+            var y = new object[] { "alpha", "beta" };
+            var result = comparer.Compare(x, y, NBi.Core.ResultSet.ColumnType.Text);
+            Assert.That(result.AreEqual, Is.True);
+        }
+
+        [Test]
+        public void Compare_NullArray_False()
+        {
+            var comparer = new ArrayComparer();
+            var x = DBNull.Value;
+            var y = new object[] { "alpha", "beta" };
+            var result = comparer.Compare(x, y, NBi.Core.ResultSet.ColumnType.Text);
+            Assert.That(result.AreEqual, Is.False);
+        }
+
+        [Test]
+        public void Compare_NullEmptyArray_True()
+        {
+            var comparer = new ArrayComparer();
+            var x = DBNull.Value;
+            var y = new object[] { };
+            var result = comparer.Compare(x, y, NBi.Core.ResultSet.ColumnType.Text);
+            Assert.That(result.AreEqual, Is.True);
+        }
+
+    }
+}
